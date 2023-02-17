0046b05fa77d1b2dc33d8f7c07257f2bc43e180b
Serito <nikitooos84@gmail.com>
2023-02-17 14:49:30 +0900

    qwe

--- app/src/main/java/com/example/punks/SlovaResult.java
+++ app/src/main/java/com/example/punks/SlovaResult.java
@@ -2,13 +2,31 @@ package com.example.punks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SlovaResult extends AppCompatActivity {
+
+    Button btn_Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slova_result);
+
+        btn_Back = findViewById(R.id.btnBack);
+
+        btn_Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SlovaResult.this, FirstActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
